445aa2aa458f5f9ee3c21a4c7d3e8b30e6c0755a
tanseanzy <tanseanzy@gmail.com>
2021-08-12 13:14:46 -0700

    connected mongoose to express

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express'),
     morgan = require('morgan'),
     app = express();
 
+require('./db/config');
+
 // Parse incoming JSON into objects
 app.use(express.json());
 // Log all requests
