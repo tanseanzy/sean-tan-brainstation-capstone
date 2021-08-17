6b36308d8da41cc65ea6507f49689b38e865f440
tanseanzy <tanseanzy@gmail.com>
2021-08-16 22:20:33 -0700

    update user route

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,3 +1,4 @@
+// OPEN CONTROLLER
 const User = require("../db/models/user");
 
 /**
@@ -20,6 +21,150 @@ exports.createUser = async (req, res) => {
       secure: process.env.NODE_ENV !== "production" ? false : true,
     });
     res.status(201).json(user);
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+  }
+};
+
+// SECURE CONTROLLER
+const User = require("../db/models/user"),
+  { sendWelcomeEmail } = require("../emails/"),
+  jwt = require("jsonwebtoken");
+
+/**
+ * @param {name, email, password}
+ * Create a user
+ * @return {user}
+ */
+exports.createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({
+      name,
+      email,
+      password,
+    });
+    const token = await user.generateAuthToken();
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: process.env.NODE_ENV !== "production" ? false : true,
+    });
+    res.status(201).json(user);
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+  }
+};
+
+/**
+ * @param {email, password}
+ * Login a user
+ * @return {user}
+ */
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: process.env.NODE_ENV !== "production" ? false : true,
+    });
+    res.json(user);
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+  }
+};
+
+//Authenticated Routes Below
+
+/**
+ * @param {req.user}
+ * Get current user
+ * @return {user}
+ */
+exports.getCurrentUser = async (req, res) => res.json(req.user);
+
+// UPDATE CURRENT USER
+// server/controllers/users.js
+
+const User = require("../db/models/user"),
+  { sendWelcomeEmail } = require("../emails/"),
+  jwt = require("jsonwebtoken");
+
+/**
+ * @param {name, email, password}
+ * Create a user
+ * @return {user}
+ */
+exports.createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = new User({
+      name,
+      email,
+      password,
+    });
+    const token = await user.generateAuthToken();
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: process.env.NODE_ENV !== "production" ? false : true,
+    });
+    res.status(201).json(user);
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+  }
+};
+
+/**
+ * @param {email, password}
+ * Login a user
+ * @return {user}
+ */
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findByCredentials(email, password);
+    const token = await user.generateAuthToken();
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "Strict",
+      secure: process.env.NODE_ENV !== "production" ? false : true,
+    });
+    res.json(user);
+  } catch (e) {
+    res.status(400).json({ error: e.toString() });
+  }
+};
+
+//Authenticated Routes Below
+
+/**
+ * @param {req.user}
+ * Get current user
+ * @return {user}
+ */
+exports.getCurrentUser = async (req, res) => res.json(req.user);
+
+/**
+ * @param {{updates}}
+ * Update a user
+ * @return {user}
+ */
+exports.updateCurrentUser = async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "avatar"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  if (!isValidOperation)
+    return res.status(400).send({ error: "invalid updates!" });
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.json(req.user);
   } catch (e) {
     res.status(400).json({ error: e.toString() });
   }
--- server/routes/secure/users.js
+++ server/routes/secure/users.js
@@ -4,3 +4,12 @@ const router = require("express").Router(),
 router.get("/me", getCurrentUser);
 
 module.exports = router;
+
+// SECURE USER ROUTES
+const router = require("express").Router(),
+  { getCurrentUser, updateCurrentUser } = require("../../controllers/users");
+
+router.get("/me", getCurrentUser);
+router.patch("/me", updateCurrentUser);
+
+module.exports = router;
